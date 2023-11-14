78197f614f04018904c61f6b8e5bd35b0a49b6ea
TejasGowda5476 <143468065+TejasGowda5476@users.noreply.github.com>
2023-11-14 09:34:00 +0530

    Update start_apache.sh

--- start_apache.sh
+++ start_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deployment
-if [ "$DEPLOYMENT_GROUP_NAME" == "new-deploy" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "new-deploy2" ]
 then
 sudo cp /var/www/html/backup-dir/index.html /var/www/html/
 sudo service httpd restart
