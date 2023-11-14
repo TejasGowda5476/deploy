6ac1f3ea778ca1624bede6212ed0c548484bf826
TejasGowda5476 <143468065+TejasGowda5476@users.noreply.github.com>
2023-11-14 09:01:11 +0530

    Update start_apache.sh

--- start_apache.sh
+++ start_apache.sh
@@ -2,6 +2,6 @@
 # Deployment
 if [ "$DEPLOYMENT_GROUP_NAME" == "new-deploy" ]
 then
-sudo cp /var/www/html/alliance-dir/index.html /var/www/html/
+sudo cp /var/www/html/backup-dir/index.html /var/www/html/
 sudo service httpd restart
 fi
