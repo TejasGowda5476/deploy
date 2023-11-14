7575b61ac532215fa1ba4e8a8ba29815b2cf5581
TejasGowda5476 <143468065+TejasGowda5476@users.noreply.github.com>
2023-11-14 09:00:09 +0530

    Update start_apache.sh

--- start_apache.sh
+++ start_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deployment
-if [ "$DEPLOYMENT_GROUP_NAME" == "mnptech-deploy" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "new-deploy" ]
 then
 sudo cp /var/www/html/alliance-dir/index.html /var/www/html/
 sudo service httpd restart
